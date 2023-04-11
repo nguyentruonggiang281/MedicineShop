d2477fbe9db950bc997343401f08858dc5ac72fa
NGUYỄN TRƯỜNG GIANG <91778158+nguyentruonggiang281@users.noreply.github.com>
2023-04-11 16:17:43 +0700

    Add files via upload

---
+++ src/main/java/com/shop/medicineshop/MedicineShopApplication.java
@@ -0,0 +1,14 @@
+package com.shop.medicineshop;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+
+@SpringBootApplication
+public class MedicineShopApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MedicineShopApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/shop/medicineshop/MedicineShopApplicationTests.java
@@ -0,0 +1,13 @@
+package com.shop.medicineshop;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MedicineShopApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
